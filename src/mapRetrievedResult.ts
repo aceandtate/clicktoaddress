0b5d9d45ace3f982840f4149599f0d8a658c16fa
silvestrevivo <silvestrevivomillan@gmail.com>
2020-02-04 16:56:35 +0100

    countryMappers updated for US formatting

--- src/mapRetrievedResult.ts
+++ src/mapRetrievedResult.ts
@@ -1,9 +1,11 @@
-import { RetrieveResponseResult, RetrieveArgs } from './types';
+import { RetrieveResponseResult, RetrieveArgs } from "./types";
 
 const isNumeric = (str: string) => !isNaN(parseFloat(str));
 
 interface CountryMappers {
-  [countryCode: string]: (result: RetrieveResponseResult) => RetrieveResponseResult;
+  [countryCode: string]: (
+    result: RetrieveResponseResult
+  ) => RetrieveResponseResult;
 }
 
 const countryMappers: CountryMappers = {
@@ -11,16 +13,24 @@ const countryMappers: CountryMappers = {
     if (isNumeric(result.building_name) && !result.building_number) {
       return {
         ...result,
-        building_name: '',
-        building_number: result.building_name,
-      }
+        building_name: "",
+        building_number: result.building_name
+      };
     }
     return result;
+  },
+  US(result: RetrieveResponseResult) {
+    return {
+      ...result,
+      postal_code: result.postal_code.split("-")[0]
+    };
   }
 } as const;
 
-
-export default function mapRetrievedResult(args: RetrieveArgs, result: RetrieveResponseResult) {
+export default function mapRetrievedResult(
+  args: RetrieveArgs,
+  result: RetrieveResponseResult
+) {
   const countryCode = args.country as keyof typeof countryMappers;
   const mapper = countryMappers[`${countryCode}`.toUpperCase()];
   return mapper ? mapper(result) : result;
