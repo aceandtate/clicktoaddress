bbc8406372607b64d872096dcd0721d8e905f7a3
Silvestre Vivo <silvestrevivomillan@gmail.com>
2020-02-04 17:21:21 +0100

    Silvestrevivo/ch11263/as a user in the us and other countries i (#9)
    * countryMappers updated for US formatting
    
    * 0.1.12

--- src/mapRetrievedResult.test.ts
+++ src/mapRetrievedResult.test.ts
@@ -1,35 +1,55 @@
-import { RetrieveArgs, RetrieveResponseResult } from './types';
-import mapRetrievedResult from './mapRetrievedResult';
+import { RetrieveArgs, RetrieveResponseResult } from "./types";
+import mapRetrievedResult from "./mapRetrievedResult";
 
-describe('mapRetrievedResult', () => {
-  it('should return same result if no mapper is required', () => {
-    const responseResult = { building_name: 'BUILDING NAME' } as RetrieveResponseResult;
+describe("mapRetrievedResult", () => {
+  it("should return same result if no mapper is required", () => {
+    const responseResult = {
+      building_name: "BUILDING NAME"
+    } as RetrieveResponseResult;
     const result = mapRetrievedResult(
-      { country: 'NL' } as RetrieveArgs,
-      responseResult,
+      { country: "NL" } as RetrieveArgs,
+      responseResult
     );
     expect(result).toBe(responseResult);
   });
 
-  it('should map Spain address\' building number and name', () => {
+  it("should map Spain address' building number and name", () => {
     const result = mapRetrievedResult(
-      { country: 'ES' } as RetrieveArgs,
-      { building_name: '1', building_number: '' } as RetrieveResponseResult,
+      { country: "ES" } as RetrieveArgs,
+      { building_name: "1", building_number: "" } as RetrieveResponseResult
     );
     expect(result).toEqual({
-      building_number: '1',
-      building_name: ''
+      building_number: "1",
+      building_name: ""
     });
   });
 
-  it('should skip mapping Spain address\' when data seems valid', () => {
+  it("should skip mapping Spain address' when data seems valid", () => {
     const result = mapRetrievedResult(
-      { country: 'ES' } as RetrieveArgs,
-      { building_name: 'x', building_number: '1' } as RetrieveResponseResult,
+      { country: "ES" } as RetrieveArgs,
+      { building_name: "x", building_number: "1" } as RetrieveResponseResult
     );
     expect(result).toEqual({
-      building_number: '1',
-      building_name: 'x'
+      building_number: "1",
+      building_name: "x"
     });
   });
-})
+
+  it("should format the post code in US without a second parameter", () => {
+    const result = mapRetrievedResult(
+      { country: "US" } as RetrieveArgs,
+      { postal_code: "30328-6228" } as RetrieveResponseResult
+    );
+
+    expect(result).toEqual({ postal_code: "30328" });
+  });
+
+  it("should not format the post code in another coutry", () => {
+    const result = mapRetrievedResult(
+      { country: "NL" } as RetrieveArgs,
+      { postal_code: "30328-6228" } as RetrieveResponseResult
+    );
+
+    expect(result).toEqual({ postal_code: "30328-6228" });
+  });
+});
